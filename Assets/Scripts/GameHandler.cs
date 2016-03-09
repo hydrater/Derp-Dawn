b54b2f523af087c161ab5d4807ac2f56c6ebf7fc
RuiPeng <ongruipeng@gmail.com>
2016-03-09 21:39:10 +0800

    Simple UI

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameHandler : MonoBehaviour {
+
+	private float HuntCooldown = 900;
+	private float HuntTimer;
+	private bool HuntReady = false;
+
+	// Use this for initialization
+	void Start () {
+		HuntTimer = HuntCooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!HuntReady) {
+			HuntTimer -= Time.deltaTime;
+			if (HuntTimer <= 0) {
+				HuntReady = true;
+				HuntTimer = 0;
+			}
+		}
+	}
+
+	public void Hunt()
+	{
+		HuntReady = false;
+		HuntTimer = HuntCooldown;
+	}
+
+	public float GetHuntTimer()
+	{
+		return HuntTimer;
+	}
+}
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+
+	GameObject canvas;
+	GameHandler gameHandler;
+	private float width;
+	private float height;
+
+	GameObject HuntButton;
+	GameObject TravelButton;
+	GameObject InventoryButton;
+
+	[SerializeField] GameObject button;
+
+	// Use this for initialization
+	void Start () {
+		this.canvas = GameObject.Find ("UICanvas");
+		this.gameHandler = GameObject.Find ("GameHandler").GetComponent<GameHandler> ();
+
+		width = canvas.GetComponent<RectTransform> ().rect.width;
+		height = canvas.GetComponent<RectTransform> ().rect.height;
+
+		Vector3 buttonPosition = new Vector3();
+		buttonPosition.Set (-width * 0.5f + button.GetComponent<RectTransform>().rect.width * 0.5f,
+		             height * 0.5f - button.GetComponent<RectTransform>().rect.height * 0.5f,
+		             0);
+
+		GameObject UIButton = (GameObject)Instantiate (button, Vector3.zero, Quaternion.identity);
+		UIButton.transform.SetParent(canvas.transform);
+		UIButton.GetComponent<RectTransform> ().localPosition = buttonPosition;
+		UIButton.GetComponentInChildren<Text> ().text = "Hunt " + gameHandler.GetHuntTimer ();
+		HuntButton = UIButton;
+
+		// Travel button
+		buttonPosition.Set (-width * 0.5f + button.GetComponent<RectTransform>().rect.width * 0.5f,
+		                    height * 0.5f - button.GetComponent<RectTransform>().rect.height * 1.5f,
+		                    0);
+		
+		UIButton = (GameObject)Instantiate (button, Vector3.zero, Quaternion.identity);
+		UIButton.transform.SetParent(canvas.transform);
+		UIButton.GetComponent<RectTransform> ().localPosition = buttonPosition;
+		UIButton.GetComponentInChildren<Text> ().text = "Travel";
+
+		TravelButton = UIButton;
+
+		// Inventory button
+		buttonPosition.Set (-width * 0.5f + button.GetComponent<RectTransform>().rect.width * 0.5f,
+		                    height * 0.5f - button.GetComponent<RectTransform>().rect.height * 2.5f,
+		                    0);
+
+		UIButton = (GameObject)Instantiate (button, Vector3.zero, Quaternion.identity);
+		UIButton.transform.SetParent(canvas.transform);
+		UIButton.GetComponent<RectTransform> ().localPosition = buttonPosition;
+		UIButton.GetComponentInChildren<Text> ().text = "Inventory";
+
+		InventoryButton = UIButton;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HuntButton.GetComponentInChildren<Text>().text = "Hunt " + (int)(gameHandler.GetHuntTimer ());
+	}
+}
